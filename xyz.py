942d34e0624450663b22c9699d251fa84b3bc1e7
elidaima <97721435+elidaima@users.noreply.github.com>
2022-02-11 11:24:04 +0300

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import streamlit as st
+st.header ('''simple web app''')
+st.write ('''*My APP*''')
+value=st.slider ('val')
+st.write (value,"squared is",value*value)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
